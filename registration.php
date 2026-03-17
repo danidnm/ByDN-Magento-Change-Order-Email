4f076a116763eba931c75179a82e6ecd9a166a0a
Daniel Navarro <danidnm@gmail.com>
2026-03-17 09:41:06 +0100

    License header

--- registration.php
+++ registration.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * @package     Bydn_ChangeOrderEmail
+ * @author      Daniel Navarro <https://github.com/danidnm>
+ * @license     GPL-3.0-or-later
+ * @copyright   Copyright (c) 2025 Daniel Navarro
+ *
+ * This file is part of a free software package licensed under the
+ * GNU General Public License v3.0.
+ * You may redistribute and/or modify it under the same license.
+ */
+
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
